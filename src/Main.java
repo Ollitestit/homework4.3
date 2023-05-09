ffb3d4d302711f718f402099544e8b1fcf79eef8
Olli <olli.konyuhova@gmail.com>
2023-05-10 01:12:05 +0500

    change Main

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         int period;
         double rate = 9.99;
         System.out.println("Срок кредита - 1 год");
-        System.out.println("Ежемесячный платеж = " + service.calculate(credit, gigperiod = 12, rate) + " руб.");
+        System.out.println("Ежемесячный платеж = " + service.calculate(credit, period = 12, rate) + " руб.");
         System.out.println();
         System.out.println("Срок кредита - 2 года");
         System.out.println("Ежемесячный платеж = " + service.calculate(credit, period = 24, rate) + " руб.");
